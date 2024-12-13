c9ffeecc6b0e2d6f17f7a881043a81b18b793f97
Aayush Niroula <ayushniroula644@gmail.com>
2024-12-13 19:19:57 +0545

    deploy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://aimscodequests.vercel.app"
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
